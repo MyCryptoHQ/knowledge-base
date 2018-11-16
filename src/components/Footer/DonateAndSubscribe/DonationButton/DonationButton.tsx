2ff4648fc119321829e543f30f1f905585524b6a
Mrtenz <maarten@zuidhoorn.com>
2018-11-16 15:21:18 +0100

    Fix donate button

--- src/components/Footer/DonateAndSubscribe/DonationButton/DonationButton.tsx
+++ src/components/Footer/DonateAndSubscribe/DonationButton/DonationButton.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
+import * as CopyToClipboard from 'react-copy-to-clipboard';
 
 interface Props {
+  text: string;
   icon: string;
   title: string;
+
+  onCopy?(): void;
 }
 
-const DonationButton: React.StatelessComponent<Props> = ({ icon, title }) => (
-  <button className="donation-button">
-    <span>
-      <img src={icon} alt={`Icon for ${title}`} /> {title}
-    </span>
-  </button>
+const DonationButton: React.StatelessComponent<Props> = ({ text, icon, title, onCopy }) => (
+  <CopyToClipboard text={text} onCopy={onCopy}>
+    <button className="donation-button">
+      <span>
+        <img src={icon} alt={`Icon for ${title}`} /> {title}
+      </span>
+    </button>
+  </CopyToClipboard>
 );
 
 export default DonationButton;
