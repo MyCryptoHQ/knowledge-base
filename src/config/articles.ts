54a3e789d3118a175f64bd69a44ebf8ea80ca438
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-02-21 15:48:42 +0100

    Remove slash

--- src/config/articles.ts
+++ src/config/articles.ts
@@ -10,5 +10,5 @@ export const POPULAR_ARTICLES: string[] = [
   'how-to/migrating/moving-from-mycrypto-to-ledger',
   'how-to/migrating/moving-from-mycrypto-to-trezor',
   'troubleshooting/tokens/adding-new-token-and-sending-custom-tokens',
-  '/general-knowledge/ethereum-blockchain/a-glossary-of-common-terms-in-the-ethereum-crypto-space'
+  'general-knowledge/ethereum-blockchain/a-glossary-of-common-terms-in-the-ethereum-crypto-space'
 ];
