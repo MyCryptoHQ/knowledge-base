7f3e8f800922b273d93566361e3d26edea613d92
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-05-09 00:39:25 +0200

    Make search more prominent on homepage (#72)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
 import FeaturedCategories from '../components/FeaturedCategories';
+import Hero from '../components/Header/Hero';
 import PopularArticles from '../components/PopularArticles';
+import Search from '../components/Search';
 import Container from '../components/ui/Container';
+import Heading from '../components/ui/Heading';
 import PageContainer from '../components/ui/PageContainer';
 import breakpoint from '../theme/breakpoints';
 
@@ -17,6 +20,10 @@ const HomeContainer = styled(Container)`
 
 const Index: FunctionComponent = () => (
   <PageContainer>
+    <Hero>
+      <Heading as="h2">How can we help you?</Heading>
+      <Search compact={false} maxWidth={'50rem'} />
+    </Hero>
     <HomeContainer>
       <FeaturedCategories />
       <PopularArticles />
