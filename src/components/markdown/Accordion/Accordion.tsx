7d7270a55b5c84b323ae41c9b8dc4f2c2e75e5e4
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-05-18 12:20:20 +0200

    Fix alert background

--- src/components/markdown/Accordion/Accordion.tsx
+++ src/components/markdown/Accordion/Accordion.tsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
+import { StyledAlert } from '../Alert';
 
 const Accordion = styled.ul`
   list-style-type: none;
   margin: 3rem 0;
   padding: 0;
+
+  ${StyledAlert} {
+    background: ${({ theme }) => theme.subHeaderBackground};
+  }
 `;
 
 export default Accordion;
