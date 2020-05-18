7e72f7cadadfd1233cb2be660b9da012006a1fda
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-05-18 16:11:28 +0200

    Fix spacing on tables and lists

--- src/components/markdown/default/Table/Table.tsx
+++ src/components/markdown/default/Table/Table.tsx
@@ -2,11 +2,11 @@ import React, { FunctionComponent } from 'react';
 import styled from 'styled-components';
 
 const StyledTable = styled.table`
-  display: block;
   width: 100%;
   overflow: auto;
   border-spacing: 0;
   border-collapse: collapse;
+  margin: 3rem 0;
 `;
 
 const Table: FunctionComponent = ({ children, ...rest }) => <StyledTable {...rest}>{children}</StyledTable>;
