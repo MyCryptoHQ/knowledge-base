338308e3ceb5caa9215eee616006666700e060d0
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-06-27 16:35:30 +0200

    Add component to generate token input data (#83)

--- src/components/markdown/index.ts
+++ src/components/markdown/index.ts
@@ -2,11 +2,13 @@ import Accordion, { AccordionItem } from './Accordion';
 import Alert from './Alert';
 import Clearfix from './Clearfix';
 import components from './default';
+import TokenInputData from './TokenInputData';
 
 export default {
   ...components,
   Accordion,
   AccordionItem,
   Alert,
-  Clearfix
+  Clearfix,
+  TokenInputData
 };
