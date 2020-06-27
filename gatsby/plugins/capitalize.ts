53cfddcfc7f5b77eb7131b476d9768adc1da3641
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-06-27 16:35:41 +0200

    Automatically capitalize titles (#84)
    * Automatically capitalize titles
    
    * Fix issue with spaces

---
+++ gatsby/plugins/capitalize.ts
@@ -0,0 +1,18 @@
+import { titleCase } from 'title-case';
+import { Node } from 'unist';
+import visit from 'unist-util-visit';
+
+interface TextNode extends Node {
+  value: string;
+}
+
+const capitalize = () => (tree: Node) => {
+  visit(tree, 'heading', node => {
+    visit<TextNode>(node, 'text', textNode => {
+      const text = textNode.value ?? '';
+      textNode.value = titleCase(text);
+    });
+  });
+};
+
+export default capitalize;
