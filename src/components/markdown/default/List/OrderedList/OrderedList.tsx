96c62f31e41694725358abd210214fc17be3ed3f
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-08-20 22:34:58 +0200

    Update list styles (#97)

--- src/components/markdown/default/List/OrderedList/OrderedList.tsx
+++ src/components/markdown/default/List/OrderedList/OrderedList.tsx
@@ -1,10 +1,21 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
 import List from '../index';
 
+const StyledOrderedList = styled(List).attrs({ as: 'ol' })`
+  list-style-type: decimal;
+
+  & & {
+    list-style-type: lower-alpha;
+  }
+
+  & & & {
+    list-style-type: lower-roman;
+  }
+`;
+
 const OrderedList: FunctionComponent = ({ children, ...rest }) => (
-  <List as="ol" {...rest}>
-    {children}
-  </List>
+  <StyledOrderedList {...rest}>{children}</StyledOrderedList>
 );
 
 export default OrderedList;
--- src/components/markdown/default/List/UnorderedList/UnorderedList.tsx
+++ src/components/markdown/default/List/UnorderedList/UnorderedList.tsx
@@ -1,10 +1,22 @@
 import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
 import List from '../index';
 
+const StyledUnorderedList = styled(List).attrs({ as: 'ul' })`
+  list-style-type: disc;
+
+  & & {
+    margin: 0;
+    list-style-type: circle;
+  }
+
+  & & & {
+    list-style-type: square;
+  }
+`;
+
 const UnorderedList: FunctionComponent = ({ children, ...rest }) => (
-  <List as="ul" {...rest}>
-    {children}
-  </List>
+  <StyledUnorderedList {...rest}>{children}</StyledUnorderedList>
 );
 
 export default UnorderedList;
