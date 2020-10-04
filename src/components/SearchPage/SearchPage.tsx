eeb97a65d17f88404c4908767dd1716e9b197f94
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2020-10-04 15:39:06 +0200

    Remove old search functionality

--- src/components/SearchPage/SearchPage.tsx
+++ src/components/SearchPage/SearchPage.tsx
@@ -1,4 +1,3 @@
-import Fuse from 'fuse.js';
 import { navigate } from 'gatsby';
 import { FunctionComponent, useMemo, useState } from 'react';
 import { useSelector } from '../../hooks';
@@ -6,47 +5,18 @@ import { Mdx } from '../../types/page';
 import PageItem from '../PageItem';
 import Heading from '../ui/Heading';
 
-const fuse = new Fuse<Mdx, Record<string, unknown>>([], {
-  keys: [
-    {
-      name: 'frontmatter.title',
-      weight: 0.4
-    },
-    {
-      name: 'frontmatter.tags',
-      weight: 0.2
-    },
-    {
-      name: 'excerpt',
-      weight: 0.4
-    }
-  ],
-  shouldSort: true,
-  location: 0,
-  threshold: 0.8
-});
-
-interface Props {
-  allPages: Mdx[];
-}
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const _paq: Array<Array<string | boolean | number>> = (typeof window !== 'undefined' && (window as any)._paq) || [];
+// const _paq: Array<Array<string | boolean | number>> = (typeof window !== 'undefined' && (window as any)._paq) || [];
 
-const SearchPage: FunctionComponent<Props> = ({ allPages }) => {
-  const searchQuery = useSelector((state) => state.navigation.searchQuery);
+const SearchPage: FunctionComponent = () => {
+  const searchQuery = useSelector(state => state.navigation.searchQuery);
   const [results, setResults] = useState<Mdx[]>([]);
 
+  // TODO
   useMemo(() => {
-    fuse.setCollection(allPages);
+    // _paq.push(['trackSiteSearch', searchQuery, false, searchResults.length]);
 
-    const searchResults = fuse.search(searchQuery, {
-      limit: 10
-    });
-
-    _paq.push(['trackSiteSearch', searchQuery, false, searchResults.length]);
-
-    setResults(searchResults);
+    setResults([]);
   }, [searchQuery]);
 
   if (searchQuery) {
