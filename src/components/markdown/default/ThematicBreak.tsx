9ddd87c272d8650276bf7925e80de26d530e5148
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2021-10-26 15:38:08 +0200

    Fix thematic break

--- src/components/markdown/default/ThematicBreak.tsx
+++ src/components/markdown/default/ThematicBreak.tsx
@@ -1,4 +1,4 @@
 import { FunctionComponent } from 'react';
 import { Divider } from '../../Divider';
 
-export const ThematicBreak: FunctionComponent = () => <Divider />;
+export const ThematicBreak: FunctionComponent = () => <Divider marginBottom="4" />;
